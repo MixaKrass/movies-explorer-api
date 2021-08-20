c5e52af82890fb01f01cb8dfe067fc01e0c1bbad
Riddler <2584275@gmail.com>
2021-08-20 16:00:35 +0300

    controller users

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+// const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const getProfile = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(() => {
+      throw new NotFoundError('Пользователь с указанным id не найден');
+    })
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch(next);
+};
+
+const updateProfile = (req, res, next) => {
+  const { email, name } = req.body;
+  User.findByIdAndUpdate(req.user._id, { email, name }, { new: true, runValidators: true })
+    .orFail(() => {
+      throw new NotFoundError('Пользователь с указанным id не найден');
+    })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        throw new BadReqError('Данные некорректны');
+      }
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getProfile, updateProfile,
+};
