af3bf46bb33f75cb668dbdd29ad3e18c961b69e7
Riddler <2584275@gmail.com>
2021-08-20 14:48:46 +0300

    router user

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { getProfile, updateProfile } = require('../controllers/users');
+
+router.get('/users/me', getProfile);
+
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), updateProfile);
+
+module.exports = router;
