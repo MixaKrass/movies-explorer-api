a914326c240c3da25a79dc06682b83c6c00e5f98
Riddler <2584275@gmail.com>
2021-10-14 21:35:48 +0300

    111

--- app.js
+++ app.js
@@ -35,11 +35,7 @@ const limiter = rateLimit({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-    return;
-  }
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 });
 
